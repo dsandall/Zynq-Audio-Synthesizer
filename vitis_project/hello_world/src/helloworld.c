afdd80861f8d00da59a9f983629500225ccb86e6
d <dsandall@calpoly.edu>
2025-02-21 23:00:06 -0800

    sd card demo and kick drum activated

--- vitis_project/hello_world/src/helloworld.c
+++ vitis_project/hello_world/src/helloworld.c
@@ -33,8 +33,6 @@
 // // }
 
 #include "platform.h"
-
-
 #include "xil_printf.h"
 #include <stdint.h>
 #include <stdio.h>
@@ -42,12 +40,17 @@
 #include "xbram.h"
 #include "xgpio.h"
 #include "xsdps.h" // SD card library
+#include "ff.h"
 
 #define LED_CHANNEL 1
 #define SWITCH_CHANNEL 2
 #define LEDS_MASK 0x0F     // 4 leds
 #define SWITCHES_MASK 0x0F // 4 switches
 #include "xparameters.h"
+
+#include "sleep.h"
+
+#include "sd_card.h"
 
 volatile uint32_t  *AudioCrtlReg = (uint32_t *) XPAR_AUDIO_CONTROL_GPIO_BASEADDR;  
 
@@ -93,15 +96,44 @@ void flickBit(int bit){
     *AudioCrtlReg ^= 0x1 << bit;
 };
 
-int main() {
+void playKick(uint8_t freq_add) {
+  int scale = 5000;
+  int pitchFall_delay = 3;
+  int volumeFall_delay = 9;
+    // high pitch for a sec
+    // low pitch for remainder
+    // long fadeout
+    //
+    writeBramVol(7);
+    writeBramFreq(1 + freq_add);
+    usleep(pitchFall_delay *scale);
+    writeBramFreq(2 + freq_add);
+    usleep(pitchFall_delay *scale);
+    writeBramFreq(3 + freq_add);
+    usleep(pitchFall_delay *scale);
+    writeBramFreq(4 + freq_add);
+    usleep(pitchFall_delay *scale);
+    writeBramVol(6);
+    usleep(volumeFall_delay*scale);
+    writeBramVol(5);
+    usleep(volumeFall_delay*scale);
+    writeBramVol(4);
+    usleep(volumeFall_delay*scale);
+    writeBramVol(3);
+    usleep(volumeFall_delay*scale);
+    writeBramVol(2);
+    usleep(volumeFall_delay*scale);
+    writeBramVol(0);
+    writeBramFreq(0);
+};
 
-  init_platform();
+int my_init() {
+
+  init_platform(); 
 
   XGpio Gpio; /* The Instance of the GPIO Driver */
   XBram Bram; /* The Instance of the BRAM Driver */
-
   XBram_Config *ConfigPtr;
-
 
   int Status;
 
@@ -126,55 +158,74 @@ int main() {
   XGpio_SetDataDirection(&Gpio, LED_CHANNEL, ~LEDS_MASK);       // output
   XGpio_SetDataDirection(&Gpio, SWITCH_CHANNEL, SWITCHES_MASK); // input
 
-  print("Hello World\n\r");
-  print("Successfully ran Hello World application");
+  return XST_SUCCESS;
+}
 
+int main() {
+
+  if (my_init() == XST_SUCCESS){
+    print("Hello World\n\r");
+  } else {
+    return XST_FAILURE;
+  }
+
+//  // write to the bram
 //   for (int i = 0; i < 16 * 4; i = i + 4) {
 //     XBram_WriteReg(XPAR_AXI_BRAM_CTRL_0_BASEADDR, i, i);
 //   }
 
-  int out_data;
-  int loop_count = 0;
 
-
-  *AudioCrtlReg = 0xFFFFFFFF;
-  xil_printf("reg is %d\n\r", *AudioCrtlReg);
   uint8_t f = 0;
   *AudioCrtlReg = 0x00000000;
   uint8_t refresh_en = 1;
   writeRefresh(refresh_en);
   uint8_t refresh_bram = 1;
   writeReg_RefreshBram(refresh_bram);
+    writePlayerVol(0x0);
+    writeBramVol(0x7);
+    writePlayerFreq(f); // samples 
+    writeBramFreq(f); //9, 12, 15 introduce glitching (but only at some vols) (and other freqs glitch at other vols..)
 
   while (1) {
 
+    // // write loop iteration to bram
     //XBram_WriteReg(XPAR_AXI_BRAM_CTRL_0_BASEADDR, 0, loop_count++);
 
+/*
+    // Read gpio switches
     u32 switches = XGpio_DiscreteRead(&Gpio, SWITCH_CHANNEL);
     XGpio_DiscreteWrite(&Gpio, LED_CHANNEL, switches);
     xil_printf("checkeddddd! %x\n\r", switches);
+*/
 
+/*
+    // read bram contents
     for (int i = 0; i < 16 * 4; i = i + 4) {
+      int out_data;
       out_data = *(volatile u32 *) ((XPAR_AXI_BRAM_CTRL_0_BASEADDR) + (i));
 
       xil_printf("%d: %X\n\r", i, out_data);
     }
+*/    
+    //xil_printf("f is: %d\n", f);
 
 
-    xil_printf("sizof int %d\n", sizeof(unsigned int));
-    
-    writePlayerVol(0x0);
-    writePlayerFreq(f); // samples 
-    
-    writeBramVol(0x7);
-    writeBramFreq(f); //9, 12, 15 introduce glitching (but only at some vols) (and other freqs glitch at other vols..)
 
-    xil_printf("bit flicked: %d\n", f);
+	int Status;
+	xil_printf("SD Polled File System Example Test \r\n");
+	Status = FfsSdPolledExample();
+	if (Status != XST_SUCCESS) {
+		xil_printf("SD Polled File System Example Test failed \r\n");
+		return XST_FAILURE;
+	}
+	xil_printf("Successfully ran SD Polled File System Example Test \r\n");
+
+
+
     xil_printf("reg is 0x%08X\n\r", *AudioCrtlReg);
 
     char c = inbyte();
     if (c == '\r') {
-
         refresh_en = refresh_en ? 0 : 1; 
         xil_printf("refresh_main_buffer is %d\n", refresh_en);
         writeRefresh(refresh_en);
@@ -183,12 +234,7 @@ int main() {
         xil_printf("refresh_bram is %d\n", refresh_bram);
         writeReg_RefreshBram(refresh_bram);  
     } else {
-
-        if (f == 15) {
-            f = 0;
-        } else {
-            f = f+1;
-        }
+        playKick(c % 8);
     }
 
     //writeRefresh(0);
@@ -204,4 +250,143 @@ int main() {
   return 0;
 }
 
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+///
+/************************** Variable Definitions *****************************/
+static FIL fil;		/* File object */
+static FATFS fatfs;
+/*
+ * To test logical drive 0, FileName should be "0:/<File name>" or
+ * "<file_name>". For logical drive 1, FileName should be "1:/<file_name>"
+ */
+static char FileName[32] = "Test.bin";
+static char *SD_File;
 
+#ifdef __ICCARM__
+#pragma data_alignment = 32
+u8 DestinationAddress[10*1024];
+#pragma data_alignment = 32
+u8 SourceAddress[10*1024];
+#else
+u8 DestinationAddress[10*1024] __attribute__ ((aligned(32)));
+u8 SourceAddress[10*1024] __attribute__ ((aligned(32)));
+#endif
+
+#define TEST 7
+MKFS_PARM mkfs_parm;
+
+
+int FfsSdPolledExample(void)
+{
+	FRESULT Res; 
+	UINT NumBytesRead;
+	UINT NumBytesWritten;
+	u32 BuffCnt;
+	BYTE work[FF_MAX_SS];
+	u32 FileSize = (8*1024);
+
+	/*
+	 * To test logical drive 0, Path should be "0:/"
+	 * For logical drive 1, Path should be "1:/"
+	 */
+	TCHAR *Path = "0:/";
+
+	for(BuffCnt = 0; BuffCnt < FileSize; BuffCnt++){
+		SourceAddress[BuffCnt] = TEST + BuffCnt;
+	}
+
+	/*
+	 * Register volume work area, initialize device
+	 */
+	Res = f_mount(&fatfs, Path, 0);
+
+	if (Res != FR_OK) {
+		return XST_FAILURE;
+	}
+
+	 mkfs_parm.fmt = FM_FAT32;
+	/*
+	 * Path - Path to logical driver, 0 - FDISK format.
+	 * 0 - Cluster size is automatically determined based on Vol size.
+	 */
+	Res = f_mkfs(Path, &mkfs_parm , work, sizeof work);
+	if (Res != FR_OK) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Open file with required permissions.
+	 * Here - Creating new file with read/write permissions. .
+	 * To open file with write permissions, file system should not
+	 * be in Read Only mode.
+	 */
+	SD_File = (char *)FileName;
+
+	Res = f_open(&fil, SD_File, FA_CREATE_ALWAYS | FA_WRITE | FA_READ);
+	if (Res) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Pointer to beginning of file .
+	 */
+	Res = f_lseek(&fil, 0);
+	if (Res) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Write data to file.
+	 */
+	Res = f_write(&fil, (const void*)SourceAddress, FileSize,
+			&NumBytesWritten);
+	if (Res) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Pointer to beginning of file .
+	 */
+	Res = f_lseek(&fil, 0);
+	if (Res) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Read data from file.
+	 */
+	Res = f_read(&fil, (void*)DestinationAddress, FileSize,
+			&NumBytesRead);
+	if (Res) {
+		return XST_FAILURE;
+	}
+
+	/*
+	 * Data verification
+	 */
+	for(BuffCnt = 0; BuffCnt < FileSize; BuffCnt++){
+		if(SourceAddress[BuffCnt] != DestinationAddress[BuffCnt]){
+			return XST_FAILURE;
+		}
+	}
+
+	/*
+	 * Close file.
+	 */
+	Res = f_close(&fil);
+	if (Res) {
+		return XST_FAILURE;
+	}
+
+	return XST_SUCCESS;
+}
---
+++ vitis_project/hello_world/src/sd_card.h
@@ -0,0 +1,8 @@
+#include "xparameters.h"	/* SDK generated parameters */
+#include "xsdps.h"		/* SD device driver */
+#include "xil_printf.h"
+#include "ff.h"
+#include "xil_cache.h"
+#include "xplatform_info.h"
+
+int FfsSdPolledExample(void);
